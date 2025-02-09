4b3d778f2e61875be65ccb5bbc6bbadc1b2e3b73
Mustafa Akgül <mustafaakguldev@icloud.com>
2025-02-09 15:08:04 +0300

    Upd base

--- credits/api/urls.py
+++ credits/api/urls.py
@@ -1,11 +1,10 @@
 from django.urls import path
-from credits.api.views import IRRTableIRRAPIView, IRRTableTableAPIView
+from credits.api.views import IRRTableTableAPIView
 
 
 app_name = 'credits'
 
 
 urlpatterns = [
-    path('create/irr', IRRTableIRRAPIView.as_view(), name='irr'),
     path('create/table', IRRTableTableAPIView.as_view(), name='table'),
 ]
