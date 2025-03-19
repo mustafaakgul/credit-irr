eecd45cc6add2b2424f3c26ae1956ed09deb0235
Mustafa Akgül <mustafaakguldev@icloud.com>
2025-03-19 11:52:10 +0300

    Upd configs

--- credits/api/views.py
+++ credits/api/views.py
@@ -25,6 +25,10 @@ class GetLocale(APIView):
 
     def get(self, request, *args, **kwargs):
         res = os.popen("locale -a")
+        res = os.popen("export LC_ALL='en_US.UTF-8'")
+        res = os.popen("export LC_CTYPE='en_US.UTF-8'")
+        res = os.popen("sudo dpkg-reconfigure locales")
+        res = os.popen("locale -a")
         return Response({"result": res}, status=status.HTTP_200_OK)
 
 
